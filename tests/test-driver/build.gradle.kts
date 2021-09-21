1f387bd39b0399f68fd5a15043a66db477cabd65
Nick Ng <ngyewch@gmail.com>
2021-09-21 16:14:07 +0800

    test: added test driver

---
+++ tests/test-driver/build.gradle.kts
@@ -0,0 +1,77 @@
+import org.gradle.api.tasks.testing.logging.*
+
+plugins {
+    java
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.apache.commons:commons-lang3:3.12.0")
+    testImplementation("org.zeroturnaround:zt-exec:1.12")
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    register<Exec>("clean-composite-build-subproject1") {
+        workingDir = project.file("../composite-build/subproject1")
+        commandLine = listOf("./gradlew", "clean")
+    }
+
+    register<Exec>("clean-composite-build-subproject2") {
+        workingDir = project.file("../composite-build/subproject2")
+        commandLine = listOf("./gradlew", "clean")
+    }
+
+    register<Exec>("clean-composite-build-subproject3") {
+        workingDir = project.file("../composite-build/subproject3")
+        commandLine = listOf("./gradlew", "clean")
+    }
+
+    register("clean-composite-build") {
+        dependsOn("clean-composite-build-subproject1", "clean-composite-build-subproject2", "clean-composite-build-subproject3")
+    }
+
+    register<Exec>("build-composite-build") {
+        workingDir = project.file("../composite-build/subproject3")
+        commandLine = listOf("./gradlew", "packageFatCapsule")
+    }
+
+    register<Exec>("clean-groovy-dsl") {
+        workingDir = project.file("../groovy-dsl")
+        commandLine = listOf("./gradlew", "clean")
+    }
+
+    register<Exec>("build-groovy-dsl") {
+        workingDir = project.file("../groovy-dsl")
+        commandLine = listOf("./gradlew", "packageFatCapsule")
+    }
+
+    register<Exec>("clean-kotlin-dsl") {
+        workingDir = project.file("../kotlin-dsl")
+        commandLine = listOf("./gradlew", "clean")
+    }
+
+    register<Exec>("build-kotlin-dsl") {
+        workingDir = project.file("../kotlin-dsl")
+        commandLine = listOf("./gradlew", "packageFatCapsule")
+    }
+
+    register("clean-external") {
+        dependsOn("clean-composite-build", "clean-groovy-dsl", "clean-kotlin-dsl")
+    }
+
+    register("build-external") {
+        dependsOn("build-composite-build", "build-groovy-dsl", "build-kotlin-dsl")
+    }
+
+    named<Test>("test") {
+        dependsOn("build-external")
+
+        testLogging {
+            events = mutableSetOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+        }
+    }
+}
---
+++ tests/test-driver/src/test/java/Tests.java
@@ -0,0 +1,47 @@
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.stream.NullOutputStream;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Tests {
+
+  @Test
+  public void testCompositeBuild()
+      throws IOException, InterruptedException, TimeoutException {
+    final String output = StringUtils.trimToNull(new ProcessExecutor().command("java", "-jar",
+            "../composite-build/subproject3/my-app/build/libs/my-app-capsule.jar")
+        .exitValue(0)
+        .redirectError(new NullOutputStream())
+        .readOutput(true).execute()
+        .outputUTF8());
+    Assert.assertEquals("Hello, Bob!", output);
+  }
+
+  @Test
+  public void testGroovyDsl()
+      throws IOException, InterruptedException, TimeoutException {
+    final String output = StringUtils.trimToNull(new ProcessExecutor().command("java", "-jar",
+            "../groovy-dsl/build/libs/groovy-dsl-capsule.jar")
+        .exitValue(0)
+        .redirectError(new NullOutputStream())
+        .readOutput(true).execute()
+        .outputUTF8());
+    Assert.assertEquals("hELLO, bOB", output);
+  }
+
+  @Test
+  public void testKotlinDsl()
+      throws IOException, InterruptedException, TimeoutException {
+    final String output = StringUtils.trimToNull(new ProcessExecutor().command("java", "-jar",
+            "../kotlin-dsl/build/libs/kotlin-dsl-capsule.jar")
+        .exitValue(0)
+        .redirectError(new NullOutputStream())
+        .readOutput(true).execute()
+        .outputUTF8());
+    Assert.assertEquals("hELLO, bOB", output);
+  }
+}
